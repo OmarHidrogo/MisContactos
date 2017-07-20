6df08c12d664b318df08a941ffbc4f4b2bea15e4
Omar Hidrogo <omar.083090@gmail.com>
2017-07-20 11:10:49 -0500

    fragment y viewpaper contacto en funcionamiento

--- app/src/main/java/com/omar_hidrogo_local/miscontactos/DetalleContacto.java
+++ app/src/main/java/com/omar_hidrogo_local/miscontactos/DetalleContacto.java
@@ -4,12 +4,17 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.URI;
 
@@ -19,6 +24,8 @@ public class DetalleContacto extends AppCompatActivity {
     private TextView tvNombre;
     private TextView tvTelefono;
     private TextView tvEmail;
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS =123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +53,61 @@ public class DetalleContacto extends AppCompatActivity {
 
     //metodo para llamar
     public void llamar(View v) {
-        String telefono = tvTelefono.getText().toString();
+
+        int permissioncheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            Intent i = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:8711363102"));
+            startActivity(i);
+        }
+
+
+       /* if(permissioncheck != PackageManager.PERMISSION_GRANTED){
+            Log.i("Mensaje", "No se tiene permiso para realizar llamadas telefonica.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 225);
+            }else {
+                    Log.i("Mensaje", "Se tiene permiso para realizar llamadas");
+            }
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                     if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)==PackageManager.PERMISSION_GRANTED){
+                    }else {
+                             ActivityCompat.requestPermissions(DetalleContacto.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_ASK_PERMISSIONS);
+                        return;
+                    }
+                    Intent i = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:8711363102"));
+                    startActivity(i);
+        }*/
+
+
+
+
+
+
+
+       /* String telefono = tvTelefono.getText().toString();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -59,7 +120,7 @@ public class DetalleContacto extends AppCompatActivity {
         }
             Intent i = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:8711363102"));
             startActivity(i);
-        //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+telefono)));
+        //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+telefono)));*/
     }
 
     //Metodo para enviar correo electronico
@@ -83,5 +144,31 @@ public class DetalleContacto extends AppCompatActivity {
                 startActivity(intent);
             }
         return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
